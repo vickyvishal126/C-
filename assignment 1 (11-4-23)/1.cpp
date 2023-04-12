05b4406813ef44ef294ae052a7b75a832f4740f9
vickyvishal126 <130487679+vickyvishal126@users.noreply.github.com>
2023-04-12 21:11:57 +0530

    Add files via upload

---
+++ assignment 1 (11-4-23)/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 70;
+    double num2 = 256.783;
+    char ch = 'A';
+
+    cout << num1 << endl;  
+    cout << num2 << endl;    
+    cout << "character: " << ch << endl;    
+    return 0;
+}
+
---
+++ assignment 1 (11-4-23)/2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter an integer: ";
+    cin >> num;   
+    cout << "The number is: " << num;
+    return 0;
+}
+
